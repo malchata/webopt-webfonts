5efc7fccb20a30cc20bbb85cd21f830530c51622
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-04-03 15:15:12 -0500

    Adding complete code and modified logo.

--- js/fontloading.js
+++ js/fontloading.js
@@ -1,3 +1,10 @@
-(function(document, window){
+document.onreadystatechange = function(){
+	var openSansLight = new FontFaceObserver("Open Sans Light"),
+		openSansRegular = new FontFaceObserver("Open Sans Regular"),
+		openSansBold = new FontFaceObserver("Open Sans Bold");
 
-})(document, window);
+	Promise.all([openSansLight.check(), openSansRegular.check(), openSansBold.check()]).then(function(){
+		document.documentElement.className += " fonts-loaded";
+		document.cookie = "fonts-loaded=";
+	});
+};
